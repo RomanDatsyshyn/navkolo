6b341f7c24cae643723d84b0008efb486eada549
Roman Datsyshyn <roman.datsyshyn@hotel-spider.com>
2022-06-01 09:06:05 +0300

    added SplashScreen for ios

--- App.js
+++ App.js
@@ -1,8 +1,13 @@
-import * as React from 'react';
+import React, {useEffect} from 'react';
+import SplashScreen from 'react-native-splash-screen';
 import {NavigationContainer} from '@react-navigation/native';
 import {RootNavigation} from './src/navigations';
 
 export default function App() {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <NavigationContainer>
       <RootNavigation />
