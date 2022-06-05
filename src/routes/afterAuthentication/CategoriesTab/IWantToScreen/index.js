f419fa619c79d1a15c18e82cf3070a1449b1e1fa
Roman Datsyshyn <roman.datsyshyn@hotel-spider.com>
2022-06-05 14:26:30 +0300

    added time set for IWantToScreen

--- src/routes/afterAuthentication/CategoriesTab/IWantToScreen/index.js
+++ src/routes/afterAuthentication/CategoriesTab/IWantToScreen/index.js
@@ -1,6 +1,8 @@
 import React, {useState, useEffect} from 'react';
 import {View, StyleSheet, Dimensions} from 'react-native';
 import Button from '../../../../components/Button';
+import DatePicker from 'react-native-date-picker';
+
 import DataService from '../../../../API/HTTP/services/data.service';
 
 const {io} = require('socket.io-client');
@@ -10,6 +12,10 @@ const w = Dimensions.get('window').width;
 import {colors} from '../../../../assets/colors';
 
 export const IWantToScreen = ({route, navigation}) => {
+  const [date, setDate] = useState(new Date());
+  const [open, setOpen] = useState(false);
+  const [isSelected, seIsSelected] = useState(false);
+
   const [userId_, setUserId_] = useState('');
   const {name, id, services} = route.params;
 
@@ -19,6 +25,7 @@ export const IWantToScreen = ({route, navigation}) => {
     socket.emit('sendUserOrderToServiceSellers', {
       userId: userId_,
       serviceId: id,
+      date,
     });
   };
 
@@ -38,8 +45,69 @@ export const IWantToScreen = ({route, navigation}) => {
     getUserRequest();
   }, []);
 
+  const isTodayOrTomorrow = (mm, dd, yyyy) => {
+    const today = new Date();
+    const tomorrow = new Date(today);
+    tomorrow.setDate(tomorrow.getDate() + 1);
+
+    let todayDate = String(today.getDate()).padStart(2, '0');
+    let tomorrowDate = String(tomorrow.getDate()).padStart(2, '0');
+
+    if (todayDate === dd) {
+      return ' Cьогодні';
+    }
+
+    if (tomorrowDate === dd) {
+      return 'Завтра';
+    }
+
+    if (todayDate !== dd && tomorrowDate !== dd) {
+      return dd + '/' + mm + '/' + yyyy;
+    }
+  };
+
+  const getFormatedDate = () => {
+    if (!isSelected) {
+      return 'Обрати дату й час';
+    } else {
+      let dd = String(date.getDate()).padStart(2, '0');
+      let mm = String(date.getMonth() + 1).padStart(2, '0');
+      let yyyy = date.getFullYear();
+
+      let hour = date.getHours() < 10 ? '0' + date.getHours() : date.getHours();
+
+      let minute =
+        date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes();
+
+      return isTodayOrTomorrow(mm, dd, yyyy) + ' - ' + hour + ' : ' + minute;
+    }
+  };
+
   return (
     <View style={styles.background}>
+      <Button label={getFormatedDate()} onPress={() => setOpen(true)} />
+      <DatePicker
+        modal
+        open={open}
+        date={date}
+        onConfirm={e => {
+          setOpen(false);
+          setDate(e);
+          seIsSelected(true);
+        }}
+        onCancel={() => {
+          setOpen(false);
+        }}
+        locale="uk"
+        title="Оберіть зручний для Вас час"
+        confirmText="Обрати"
+        cancelText="Скасувати"
+        minimumDate={new Date()}
+        minuteInterval={5}
+      />
+
+      <View style={styles.spacing} />
+
       <Button
         label={name}
         onPress={() => {
@@ -51,9 +119,7 @@ export const IWantToScreen = ({route, navigation}) => {
         pink
         bold
       />
-
       <View style={styles.spacing} />
-
       <Button
         label={'Повернутися назад'}
         onPress={() =>
